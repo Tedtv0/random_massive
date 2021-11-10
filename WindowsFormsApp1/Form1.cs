30f77f78e205bbc4b92e805265dd1e5a0d09e824
Nikita Lobanov <tedtv@yandex.ru>
2021-11-10 13:31:31 +0500

    2 zadachi

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string s = textBox1.Text;
+            int a = Convert.ToInt32(s);
+            for (int i = 0; i < a; i++)
+            {
+                double val = rnd.NextDouble() * (0.1) + 1.0;
+                val = val * 1000;
+                var p = Math.Truncate(val);
+                var t = val - p;
+                p = t * 1000;
+                t = Math.Truncate(p);
+                textBox2.Text = textBox2.Text + " " + Convert.ToString(t);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ rm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        Random rand = new Random();
+        Console.WriteLine("Сколько чисел вывести?");
+        string s = Console.ReadLine();
+        int a = Convert.ToInt32(s);
+        for (int i = 0; i < a; i++)
+        {
+            string stepValue = Convert.ToInt32(Math.Pow(a, 2)).ToString();
+            int startIndex = stepValue.Length / 4;
+            int b = a.ToString().Length;            
+            Console.WriteLine(rand.Next(100));
+        }
+    }
+}
